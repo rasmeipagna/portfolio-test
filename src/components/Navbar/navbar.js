98bf7b267aac61eda8f7024b63f73f4267b49c71
RasmeiPagna <rasmeipagna.toung@gmail.com>
2023-08-23 15:30:19 +0200

    Initial commit

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <img src="" alt=""/>
+        
+    </nav>
+  )
+}
+
+export default Navbar;
