15d68a58833647c18499207d3fa421695671a1fa
RasmeiPagna <rasmeipagna.toung@gmail.com>
2023-08-23 15:42:21 +0200

    update logo

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import logo from '../../assets/logo.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <img src="" alt=""/>
+        <img src={logo} alt="logo" className="logo"/>
+        <div className="desktopMenu">
+          <button className="desktopMenu">
+            <img src="" alt="" className="destokMenuImg" />Contact me</button>
+
+
+        </div>
         
     </nav>
   )
